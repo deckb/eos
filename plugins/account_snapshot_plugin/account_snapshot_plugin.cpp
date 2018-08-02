95c28321ccf4610a66f312b5afcb3ce160582e9c
Buddy Deck <deck@eosnewyork.io>
2018-08-02 00:02:31 +0000

    add recursive check of action_traces cleanup files

--- plugins/account_snapshot_plugin/account_snapshot_plugin.cpp
+++ plugins/account_snapshot_plugin/account_snapshot_plugin.cpp
@@ -11,22 +11,21 @@
 #include <eosio/chain_plugin/chain_plugin.hpp>
 #include <iostream>
 #include <fstream>
+#include <boost/thread/mutex.hpp>
 
 namespace eosio {
   using namespace chain;
   using boost::signals2::scoped_connection;
-
   static appbase::abstract_plugin& _account_snapshot_plugin = app().register_plugin<account_snapshot_plugin>();
 
   class account_snapshot_plugin_impl {
   public:
     chain_plugin*          chain_plug = nullptr;
-
-    //static const int64_t DEFAULT_TRANSACTION_TIME_LIMIT;
-    //int64_t transactions_time_limit = DEFAULT_TRANSACTION_TIME_LIMIT;
     
     fc::microseconds       abi_serializer_max_time;
-
+    fc::optional<scoped_connection> accepted_block_connection;
+    fc::optional<scoped_connection> irreversible_block_connection;
+    fc::optional<scoped_connection> accepted_transaction_connection;
     fc::optional<scoped_connection> applied_transaction_connection;
     
     account_snapshot_plugin_impl()
@@ -44,28 +43,41 @@ namespace eosio {
       account_file.flush();
       account_file.close();
     }
-      
+    
     void on_applied_transaction( const transaction_trace_ptr& trace )
     {
-      for( const auto& atrace : trace->action_traces )
-	{
-	  on_action_trace( atrace );
-	}
+      process_account_traces(trace->action_traces);
     }
 
+    void process_account_traces( const vector<action_trace> traces)
+    {
+      for( const auto& atrace : traces )
+      {
+	on_action_trace( atrace );
+	process_account_traces(atrace.inline_traces);
+      }
+    }
+    
     void on_action_trace( const action_trace& at )
     {
-      if (at.act.name == NEW_ACCOUNT) {
-	const auto create = at.act.data_as<chain::newaccount>();
-	//ilog("Created ${u}", ("u",create.name));
+      write_account(at.act);
+    }
+    
+    void write_account(const action& act)
+    {
+      if (act.name == NEW_ACCOUNT) {
+	const auto create = act.data_as<chain::newaccount>();
+	ilog("Created ${u}", ("u",create.name));
+	boost::mutex::scoped_lock lock(flock);
 	account_file << create.name.to_string() << std::endl;
 	account_file.flush();
       }
     }
-
+    
   private:
     const account_name NEW_ACCOUNT = "newaccount";
     std::ofstream account_file;
+    boost::mutex flock;
   };
 
   account_snapshot_plugin::account_snapshot_plugin():my(new account_snapshot_plugin_impl()){}
@@ -90,12 +102,24 @@ namespace eosio {
     std::string fo = options.at( "account-snapshot-file" ).as<std::string>();
 
     my->open_log_file(fo);
-    
+   
     auto& chain = my->chain_plug->chain();
+    /*
+    my->accepted_block_connection.emplace( chain.accepted_block.connect( [&]( const chain::block_state_ptr& bs ) {
+	  my->accepted_block( bs );
+    } ));
 
+    my->irreversible_block_connection.emplace( chain.irreversible_block.connect( [&]( const chain::block_state_ptr& bs ) {
+      	  my->applied_irreversible_block( bs );
+    } ));
+    
+    my->accepted_transaction_connection.emplace( chain.accepted_transaction.connect( [&]( const chain::transaction_metadata_ptr& t ) {
+	  my->on_accepted_transaction( t );
+    } ));
+    */
     my->applied_transaction_connection.emplace(chain.applied_transaction.connect( [&]( const transaction_trace_ptr& p ){
 	  my->on_applied_transaction(p);
-	}));
+    }));
   }
 
   void account_snapshot_plugin::plugin_startup() {

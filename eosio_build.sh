7bc0365237e3610e617c205e82685c96ef787087
Buddy Deck <deck@eosnewyork.io>
2018-09-21 15:45:35 -0400

    update build script

--- eosio_build.sh
+++ eosio_build.sh
@@ -258,38 +258,14 @@
       -DCMAKE_C_COMPILER="${C_COMPILER}" -DWASM_ROOT="${WASM_ROOT}" -DCORE_SYMBOL_NAME="${CORE_SYMBOL_NAME}" \
       -DOPENSSL_ROOT_DIR="${OPENSSL_ROOT_DIR}" -DBUILD_MONGO_DB_PLUGIN=true \
       -DENABLE_COVERAGE_TESTING="${ENABLE_COVERAGE_TESTING}" -DBUILD_DOXYGEN="${DOXYGEN}" \
-      -DCMAKE_INSTALL_PREFIX="/usr/local/eosio" "${SOURCE_DIR}"
+      -DCMAKE_INSTALL_PREFIX="/usr/local/eosio" \
+      -DBUILD_ACCOUNT_SNAPSHOT_PLUGIN=true \
+      "${SOURCE_DIR}"
    then
       printf "\\n\\t>>>>>>>>>>>>>>>>>>>> CMAKE building EOSIO has exited with the above error.\\n\\n"
       exit -1
    fi
 
-	if [ ! -d "${BUILD_DIR}" ]; then
-		if ! mkdir -p "${BUILD_DIR}"
-		then
-			printf "Unable to create build directory %s.\\n Exiting now.\\n" "${BUILD_DIR}"
-			exit 1;
-		fi
-	fi
-	
-	if ! cd "${BUILD_DIR}"
-	then
-		printf "Unable to enter build directory %s.\\n Exiting now.\\n" "${BUILD_DIR}"
-		exit 1;
-	fi
-	
-	if [ -z "$CMAKE" ]; then
-		CMAKE=$( command -v cmake )
-	fi
-	
-	if ! "${CMAKE}" -DCMAKE_BUILD_TYPE="${CMAKE_BUILD_TYPE}" -DCMAKE_CXX_COMPILER="${CXX_COMPILER}" \
-		-DCMAKE_C_COMPILER="${C_COMPILER}" -DWASM_ROOT="${WASM_ROOT}" -DCORE_SYMBOL_NAME="${CORE_SYMBOL_NAME}" \
-		-DOPENSSL_ROOT_DIR="${OPENSSL_ROOT_DIR}" -DBUILD_MONGO_DB_PLUGIN=true \
-		-DENABLE_COVERAGE_TESTING="${ENABLE_COVERAGE_TESTING}" -DBUILD_DOXYGEN="${DOXYGEN}" -DCMAKE_INSTALL_PREFIX="/usr/local/eosio" $LOCAL_CMAKE_FLAGS ..
-	then
-		printf "\\n\\t>>>>>>>>>>>>>>>>>>>> CMAKE building EOSIO has exited with the above error.\\n\\n"
-		exit -1
-	fi
    if [ "${START_MAKE}" == "false" ]; then
       printf "\\n\\t>>>>>>>>>>>>>>>>>>>> EOSIO has been successfully configured but not yet built.\\n\\n"
       exit 0
